42ac6f67177ae6fac7e6106235764d4110523647
Brandon Lew <brandon.t.lew@gmail.com>
2017-03-04 23:57:38 -0800

    updated icon and nav bar

--- js/main.js
+++ js/main.js
@@ -1,19 +1,52 @@
 $(document).ready(function() {
+
+//-----TOGGLE BG COLOR-----//
   $('#bg_button').click(function() {
     $('#bg_white').toggleClass('bg_black');
   });
+
+//-----PAGE SCROLLING-----//
+  
+  $('a.page-scroll').bind('click', function(event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+          scrollTop: $($anchor.attr('href')).offset().top
+      }, 1500, 'easeInOutExpo');
+      event.preventDefault();
+  });
 });
 
-var myVar;
+//-----NAVBAR-----//
 
-function myFunction() {
-  myVar = setTimeout(showPage, 1000);
+function collapseNavbar() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
 }
 
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
-}
+$(window).scroll(collapseNavbar);
+$(document).ready(collapseNavbar);
+
+$('.navbar-collapse ul li a').click(function() {
+    $(this).closest('.collapse').collapse('toggle');
+});
+
+//-----LOADER-----//
+
+//var myVar;
+//
+//function myFunction() {
+//  myVar = setTimeout(showPage, 1000);
+//}
+//
+//function showPage() {
+//  document.getElementById("loader").style.display = "none";
+//  document.getElementById("myDiv").style.display = "block";
+//}
+
+//-----MODAL-----//
 
 function onClick(element) {
   document.getElementById("modal01").style.display = "block";
