6818e276648eac1118e6a2d4da314a0ded3d7079
Brandon Lew <brandon.t.lew@gmail.com>
2017-03-07 20:17:53 -0800

    added scroll reveal animations

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@ $(document).ready(function() {
   "use strict";
 
 // TOGGLE BG COLOR
-  $('#bg_button').click(function() {
-    $('#bg_white').toggleClass('bg_black');
+  
+  $('.bg_button').click(function() {
+    $('.bg_white').toggleClass('bg_black');
   });
 
 // PAGE SCROLLING FEATURE
@@ -17,24 +18,39 @@ $(document).ready(function() {
   });
   
 // CLOSE THE RESPONSIVE MENU ON CLICK
+  
   $('.navbar-collapse ul li a').click(function() {
       $('.navbar-toggle:visible').click();
   });
   
 // OFFSET MAIN NAV  
+  
   $('#mainNav').affix({
     offset: {
         top: 100
     }
   });
   
+// SCROLL REVEAL ANIMATION  
+  
+  window.sr = ScrollReveal();
+  sr.reveal('.sr_button', {
+    duration: 1000,
+    delay: 300
+  });
+  sr.reveal('.sr_icon', {
+    duration: 500,
+    delay: 500,
+    distance: '0px',
+    scale: 0.2,
+  }, 300);
+  
 });
 
-
-
-//-----MODAL-----//
+// MODAL
 
 function onClick(element) {
+  "use strict";
   document.getElementById("modal01").style.display = "block";
   document.getElementById("img01").src = element.src;
   document.getElementById("caption").innerHTML = element.alt;
