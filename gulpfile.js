d31b2b4bf4c40f9055d2e03485683cd006455c95
Brandon Lew <brandon.t.lew@gmail.com>
2018-01-13 00:44:05 -0800

    add gulp modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,45 @@
 var gulp = require('gulp');
 
 // MODULES
+var useref = require('gulp-useref');
+var gulpif = require('gulp-if');
 var jsMinify = require('gulp-uglify');
 var cssMinify = require('gulp-cssmin');
 var rename = require('gulp-rename');
 
 // LOCATION VARIABLES
-var srcDir = '';
-var newDir = '';
+// var srcDir = '';
+// var newDir = '';
 
 //----------
 // TASKS
 //----------
 
-// MINIFY JAVASCRIPT FILES
-gulp.task('js', function() {
-  return gulp.src(['' + srcDir + 'js/*.js','!' + srcDir + 'js/*.min.js'])
-    .pipe(jsMinify())
-    .pipe(rename({ extname: '.min.js' }))
-    .pipe(gulp.dest('' + srcDir + 'js/'));
+// PARSE HTML, MINIFY, AND COPY TO NEW LOCATION
+gulp.task('html', function() {
+  return gulp.src('index-staging.html')
+    .pipe(useref())
+    .pipe(gulpif(['js/*.js','js/**/*.js'], jsMinify()))
+    .pipe(gulpif('css/*.css', cssMinify()))
+    .pipe(gulpif('index-staging.html', rename('index.html')))
+    .pipe(gulp.dest(''));
 });
 
-// MINFIY CSS FILES
-gulp.task('css', function() {
-  return gulp.src(['' + srcDir + 'css/*.css','!' + srcDir + 'css/*.min.css'])
-    .pipe(cssMinify())
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('' + srcDir + 'css/'));
-});
+// // MINIFY JAVASCRIPT FILES
+// gulp.task('js', function() {
+//   return gulp.src(['' + srcDir + 'js/*.js','!' + srcDir + 'js/*.min.js'])
+//     .pipe(jsMinify())
+//     .pipe(rename({ extname: '.min.js' }))
+//     .pipe(gulp.dest('' + srcDir + 'js/'));
+// });
+//
+// // MINFIY CSS FILES
+// gulp.task('css', function() {
+//   return gulp.src(['' + srcDir + 'css/*.css','!' + srcDir + 'css/*.min.css'])
+//     .pipe(cssMinify())
+//     .pipe(rename({ extname: '.min.css' }))
+//     .pipe(gulp.dest('' + srcDir + 'css/'));
+// });
 
 // RUN MINIFY TASKS
-gulp.task('default', ['js','css'], function() {});
+gulp.task('default', ['html'], function() {});
