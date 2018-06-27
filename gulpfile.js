f140f3cb474cb9a69ae4eed0058b38a5e462d41a
Brandon Lew <brandon.t.lew@gmail.com>
2018-06-26 20:17:38 -0700

    update code to es6 and compatible with gulp 4.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,29 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
 
 // MODULES
-var useref = require('gulp-useref');
-var gulpif = require('gulp-if');
-var jsMinify = require('gulp-uglify');
-var cssMinify = require('gulp-cssmin');
-var rename = require('gulp-rename');
-
-// LOCATION VARIABLES
-// var srcDir = '';
-// var newDir = '';
+const useref = require('gulp-useref');
+const gulpif = require('gulp-if');
+const jsMinify = require('gulp-uglify');
+const cssMinify = require('gulp-cssmin');
+const rename = require('gulp-rename');
 
 //----------
 // TASKS
 //----------
 
 // PARSE HTML, MINIFY, AND COPY TO NEW LOCATION
-gulp.task('html', function() {
+gulp.task('html', () => {
   return gulp.src('index-staging.html')
     .pipe(useref())
     .pipe(gulpif(['js/*.js','js/**/*.js'], jsMinify()))
     .pipe(gulpif('css/*.css', cssMinify()))
     .pipe(gulpif('index-staging.html', rename('index.html')))
-    .pipe(gulp.dest(''));
+    .pipe(gulp.dest( (file) => {
+      return file.base
+    }));
 });
 
-// // MINIFY JAVASCRIPT FILES
-// gulp.task('js', function() {
-//   return gulp.src(['' + srcDir + 'js/*.js','!' + srcDir + 'js/*.min.js'])
-//     .pipe(jsMinify())
-//     .pipe(rename({ extname: '.min.js' }))
-//     .pipe(gulp.dest('' + srcDir + 'js/'));
-// });
-//
-// // MINFIY CSS FILES
-// gulp.task('css', function() {
-//   return gulp.src(['' + srcDir + 'css/*.css','!' + srcDir + 'css/*.min.css'])
-//     .pipe(cssMinify())
-//     .pipe(rename({ extname: '.min.css' }))
-//     .pipe(gulp.dest('' + srcDir + 'css/'));
-// });
-
-// RUN MINIFY TASKS
-gulp.task('default', ['html'], function() {});
+// RUN TASKS
+gulp.task('default', gulp.series(['html'], (done) => {
+  done();
+}));
