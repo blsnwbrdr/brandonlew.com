4633f0e98b6e6cce92f3c07b0c2845fec5f3c691
Brandon Lew <brandon.t.lew@gmail.com>
2020-11-02 21:23:21 -0500

    remove lab

--- lab/quiz/savejson.php
+++
@@ -1,6 +0,0 @@
-<?php
-$scores = json_decode(file_get_contents("scores.json"));
-$data = json_decode($_GET["data"]);
-array_push($scores,$data);
-file_put_contents("scores.json", json_encode($scores))
-?>
