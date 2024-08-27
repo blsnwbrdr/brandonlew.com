ee42a60170cc133c2d6e745bfb3d9b9beb415d87
Brandon Lew <25602300+blsnwbrdr@users.noreply.github.com>
2024-08-27 09:42:41 -0700

    add geowiz import and cspell words

---
+++ gulp-import-geowiz.js
@@ -0,0 +1,25 @@
+// GULP
+const gulp = require('gulp');
+
+// MODULES
+const clean = require('gulp-clean');
+
+// FILE LOCATIONS
+const build = '../geowiz-web/dist/geowiz-web/browser/**/*';
+const portfolio = 'portfolio/geowiz/';
+
+//----------
+// TASKS
+//----------
+
+// DELETE PORTFOLIO DIRECTORY
+gulp.task('delete', () => {
+  return gulp.src([portfolio], { force: true, read: false }).pipe(clean());
+});
+
+// COPY BUILD FILES TO PORTFOLIO DIRECTORY
+gulp.task('move', () => {
+  return gulp.src([build]).pipe(gulp.dest(portfolio));
+});
+
+gulp.task('default', gulp.series(['delete', 'move']), function () {});
